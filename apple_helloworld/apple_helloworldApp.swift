63150fafcb1d5b1bc7196b2b859cfc87b1a67e04
Matt Spinks <matthew.spinks@dps.ok.gov>
2023-01-30 11:35:36 -0600

    Initial Commit

---
+++ apple_helloworld/apple_helloworldApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_helloworldApp.swift
+//  apple_helloworld
+//
+//  Created by bearcave on 1/30/23.
+//
+
+import SwiftUI
+
+@main
+struct apple_helloworldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
